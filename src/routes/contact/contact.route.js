07a8493aeae27f74dbb1dd6891ef2d7b22c80457
Biswajeet <biswajeetpadhi1999@gmail.com>
2023-07-16 12:27:46 +0530

    Added /ping route

--- src/routes/contact/contact.route.js
+++ src/routes/contact/contact.route.js
@@ -1,6 +1,9 @@
 const route = require("express").Router();
 const contactController = require("./contact.controller");
 
+route.get('/ping', (req, res) => {
+   res.send("healthy...");
+});
 route.post("/identify", contactController.verify);
 
 module.exports = route;
