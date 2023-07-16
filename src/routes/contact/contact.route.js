72e02c27cc8cf43ee2cada3e9364bd4787b2c314
Biswajeet <biswajeetpadhi1999@gmail.com>
2023-07-16 13:29:36 +0530

    updated start script to support cyclic deployment

--- src/routes/contact/contact.route.js
+++ src/routes/contact/contact.route.js
@@ -1,8 +1,8 @@
 const route = require("express").Router();
 const contactController = require("./contact.controller");
 
-route.get('/ping', (req, res) => {
-   res.send("healthy...");
+route.get("/ping", (req, res) => {
+  res.status(200).json({ message: "healthy..." });
 });
 route.post("/identify", contactController.verify);
 
